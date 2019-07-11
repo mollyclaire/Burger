8eaed1637d292798591ccd34c655f4e59c628ef7
Molly Graham <mollyclaire.graham@gmail.com>
2019-07-11 17:14:28 -0500

    Added some styling to static HTML page.

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ USE burgers_db;
 -- Create the table tasks.
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
-  burger_name varchar(100) NOT NULL,
-  devoured BOOLEAN,
+  name varchar(100) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
